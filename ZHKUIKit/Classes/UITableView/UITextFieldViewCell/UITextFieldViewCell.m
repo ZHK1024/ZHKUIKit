f47320d173cf6e25c3e2d1c6782b1756ebf45e2b
TongJunfeng <389914070@qq.com>
2020-03-20 15:56:57 +0800

    补充

--- ZHKUIKit/Classes/UITableView/UITextFieldViewCell/UITextFieldViewCell.m
+++ ZHKUIKit/Classes/UITableView/UITextFieldViewCell/UITextFieldViewCell.m
@@ -14,7 +14,7 @@
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
-        [self setupUI];
+        [self _setupUI];
         [self didInit];
     }
     return self;
